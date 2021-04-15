555ec61d761d5409c32213ccd8f7ba21bf7f9411
Gregor Adams <gregor.adams@evernest.com>
2021-04-15 08:30:02 +0200

    feat(switch): use automatic iframe resize feature

--- src/pages/switch/index.tsx
+++ src/pages/switch/index.tsx
@@ -2,39 +2,42 @@ import Spaced from "@/components/spaced";
 import withSDK from "@/utils/with-sdk";
 import { FieldExtensionSDK } from "@contentful/app-sdk";
 import { Switch } from "@contentful/forma-36-react-components";
+import { NextPage } from "next";
 import React from "react";
 
+// Ensure the correct sdk type definition
 export interface AppProps {
 	sdk: FieldExtensionSDK;
 }
 
-const SwitchApp: React.FC<AppProps> = ({ sdk }) => {
+const SwitchApp: NextPage<AppProps> = ({ sdk }) => {
+	// Store the field values locally
 	const [fieldValue, setFieldValue] = React.useState(sdk.field.getValue());
 
 	React.useEffect(() => {
 		const subscribe = () => {
-			// Enable auto-resizer
-			sdk.window.startAutoResizer();
-
-			// Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+			// Handler(s) for external field value changes.
+			// (e.g. when multiple authors are working on the same entry)
+			// If you are changing multiple/sibling fields you should add handlers for them here.
 			const detachExternalChange = sdk.field.onValueChanged(value => {
 				setFieldValue(value);
 			});
 
 			// Return unsubscribe
 			return () => {
-				// Disable auto-resizer
-				sdk.window.stopAutoResizer();
-
 				// Detach external changes
+				// If you are changing multiple/sibling fields you should detach them here.
 				detachExternalChange();
 			};
 		};
 
-		// Subscribe and return the unsubscribe handler
+		// Subscribe and return the unsubscribe handler(s)
 		return subscribe();
 	}, [sdk]);
 
+	// Return your app here.
+	// We wrap the app in a Wrapper with spacing to ensure that focus rings are visible.
+	// Feel free to remove the Wrapper (<Spaced/>) if you want to handle this on your own.
 	return (
 		<Spaced>
 			<Switch
@@ -42,7 +45,10 @@ const SwitchApp: React.FC<AppProps> = ({ sdk }) => {
 				labelText="Yes"
 				isChecked={fieldValue}
 				onToggle={bool => {
+					// Save the local state of the field value
 					setFieldValue(bool);
+					// Save the state of the field value on contentful
+					// This will update the local (unpublished) change on contenful
 					void sdk.field.setValue(bool);
 				}}
 			/>
@@ -50,4 +56,5 @@ const SwitchApp: React.FC<AppProps> = ({ sdk }) => {
 	);
 };
 
+// Decorate the app with the contenful sdk and define the type of sdk here.
 export default withSDK<FieldExtensionSDK>(SwitchApp);
